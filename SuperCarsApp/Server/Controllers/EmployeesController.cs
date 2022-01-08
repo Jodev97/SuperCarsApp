a17aa49220dd3b084d6d5c3e3534cf610174948a
Jairo Andres <jairoquinterodev@gmail.com>
2022-01-08 10:11:31 -0500

    first commit

---
+++ SuperCarsApp/Server/Controllers/EmployeesController.cs
@@ -0,0 +1,62 @@
+﻿using SuperCarsApp.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SuperCarsApp.Server.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MarketPlace.Server.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeesController: ControllerBase
+    {
+
+        private readonly ApplicationDbContext context;
+        public EmployeesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> Get()
+        {
+            
+            return await context.Employees.ToListAsync();
+        }
+        [HttpGet("{id}",Name = "getEmployee")]
+        public async Task<ActionResult<Employee>> Get(int id)
+        {
+            
+            return await context.Employees.FirstOrDefaultAsync(x => x.Id == id);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post(Employee employee)
+        {
+          
+            context.Add(employee);
+            await context.SaveChangesAsync();
+            return new CreatedAtRouteResult("getEmployee", new { id = employee.Id }, employee);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var employee = new Employee { Id = id };
+            context.Remove(employee);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpPut]
+        public async Task<ActionResult> Put(Employee employee)
+        {
+
+            context.Entry(employee).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+       
+    }
+}
---
+++ SuperCarsApp/Shared/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace SuperCarsApp.Shared
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El Nombre es requerido")]
+        public string Name { get; set; }
+    }
+}
